545ae9d0a3a7adc58fbd611f7b2933420bf50db9
Hang Do <doh25@cs.washington.edu>
2020-10-18 21:20:40 +0700

    React worked for now!

--- workdatebuddies/frontend/src/App.js
+++ workdatebuddies/frontend/src/App.js
@@ -4,44 +4,13 @@ import './App.css';
 
 class App extends React.Component {
 
-  state = { 
-    details : ["hello", "world"], 
-  } 
-
-  // componentDidMount() { 
-
-  //   let data ; 
-
-  //   axios.get('http://localhost:8000/wel/') 
-  //   .then(res => { 
-  //       data = res.data; 
-  //       this.setState({ 
-  //           details : data     
-  //       }); 
-  //   }) 
-  //   .catch(err => {}) 
-  // } 
-
-  render() { 
+  render() {
     return( 
       <div> 
-            {this.state.details.map((detail, id) =>  ( 
-            <div key={id}> 
-            <div > 
-                  <div > 
-                        <h1>{detail.detail} </h1> 
-                        <footer >--- by 
-                        <cite title="Source Title"> 
-                        {detail.name}</cite> 
-                        </footer> 
-                  </div> 
-            </div> 
-            </div> 
-            ) 
-        )} 
+        Hello World!
       </div> 
-    ); 
-  } 
+      );
+  }
 }
 
 export default App;
